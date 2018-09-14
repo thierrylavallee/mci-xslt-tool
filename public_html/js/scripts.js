50d0d71645e0e2a756eca2a91887133a6a2386b0
Lavallée <thlv@EAW15ML210.local>
2018-09-14 21:44:19 +0200

    up

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             orientation: 'landscape',
             unit: 'pt',
             format: [297 * 2.83465, 210 * 2.83465]}),
-                source = $("#testPage")[0],
+                source = $("#result")[0],
                 margins = {
                     top: 20 * 2.83465,
                     bottom: 20 * 2.83465,
